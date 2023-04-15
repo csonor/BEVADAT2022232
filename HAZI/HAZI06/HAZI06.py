6d3525865c00a648539029749f784b180f41a717
csonor <csdoba2@gmail.com>
2023-04-15 10:11:33 +0200

    changed to best accuracy

--- HAZI/HAZI06/HAZI06.py
+++ HAZI/HAZI06/HAZI06.py
@@ -11,7 +11,7 @@ Y = data.iloc[:, -1].values.reshape(-1, 1)
 X_train, X_test, Y_train, Y_test = train_test_split(
     X, Y, test_size=.2, random_state=41)
 
-classifier = DecisionTreeClassifier(120, 12)
+classifier = DecisionTreeClassifier(90, 11)
 classifier.fit(X_train, Y_train)
 Y_pred = classifier.predict(X_test)
 print(accuracy_score(Y_test, Y_pred))
